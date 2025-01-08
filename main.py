04386593ede63e5da85e4cde1e5bdbcc30f83d1f
shinsuheon <24_11113@daejin.sen.hs.kr>
2025-01-08 11:21:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ st.write('제 이메일 주소는 안알려줄거야')
 
 st.button("처음으로 되돌리다", type="primary")
 if st.button("뽑다 무작위 숫자를"):
-    st.write("random.random()")
+    st.write(random.random())
 else:
     st.write("좋은안녕")
 
