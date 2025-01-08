552d830e5e7f7db5c6bb42574cf673e40102a3a6
shinsuheon <24_11113@daejin.sen.hs.kr>
2025-01-08 11:32:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -46,19 +46,3 @@ st.write("자살율 데이터:")
 st.dataframe(data)
 
 
-# Add histogram data
-x1 = np.random.randn(200) - 2
-x2 = np.random.randn(200)
-x3 = np.random.randn(200) + 2
-
-# Group data together
-hist_data = [x1, x2, x3]
-
-group_labels = ['Group 1', 'Group 2', 'Group 3']
-
-# Create distplot with custom bin_size
-fig = ff.create_distplot(
-        hist_data, group_labels, bin_size=[.1, .25, .5])
-
-# Plot!
-st.plotly_chart(fig, use_container_width=True)
