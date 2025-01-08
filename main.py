294e03ed94ed54c6aa1bbbfa3937b24aed7f4a52
shinsuheon <24_11113@daejin.sen.hs.kr>
2025-01-08 11:17:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,12 @@ st.title("나의 첫번째 앱")
 st.text('\n\n\n\n\n\n')
 st.write('ㅎㅇㅎㅇㅎㅇ')
 st.write('제 이메일 주소는 안알려줄거야')
-st.button(label, key=None, help=None, on_click=None, args=None, kwargs=None, *, type="secondary", icon=None, disabled=False, use_container_width=False)
+
+st.button("Reset", type="primary")
+if st.button("Say hello"):
+    st.write("Why hello there")
+else:
+    st.write("Goodbye")
+
+if st.button("Aloha", type="tertiary"):
+    st.write("Ciao")
