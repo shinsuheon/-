6e9a697475b00f06f649f90320d66e41642a9909
shinsuheon <24_11113@daejin.sen.hs.kr>
2025-01-08 11:22:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ st.button("처음으로 되돌리다", type="primary")
 if st.button("뽑다 무작위 숫자를"):
     st.write(random.random())
 else:
-    st.write("좋은안녕")
+    st.write("힘세고 강한 아침")
 
-if st.button("알로하", type="tertiary"):
-    st.write("챠오")
+if st.button("나는 왈도", type="tertiary"):
+    st.write("그것은 굉장한")
